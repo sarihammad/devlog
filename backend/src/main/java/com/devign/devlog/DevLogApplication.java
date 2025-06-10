34c27aa92d84bad57bb12a973ee69887b65c46ab
Sari Hammad <sari_hammad@hotmail.com>
2025-06-10 19:13:10 -0400

    feat: init Spring backend

---
+++ backend/src/main/java/com/devign/devlog/DevLogApplication.java
@@ -0,0 +1,13 @@
+package com.devign.devlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevLogApplication.class, args);
+	}
+
+}
